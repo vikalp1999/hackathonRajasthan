614f5bf4b8fe03421185bac62389476aec928cc0
vikalp1999 <shandilyavikalp@gmail.com>
2023-03-15 23:58:53 +0530

    make test and vaccination model

--- backend/src/model/user.mode.js
+++ backend/src/model/user.mode.js
@@ -1,5 +1,42 @@
 const mongoose= require("mongoose");
 
 const UserSchema= mongoose.Schema({
-    
+    name:{
+        type:String,
+        required:true,
+    },
+    mobile_number:{
+       type:Number,
+       required:true
+    },
+    age:{
+        type:Number,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    adhar_number:{
+        type:Number,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["doctor",
+            "user"],
+        default:"user"
+    },
+    vaccine:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"vaccine"
+    }],
+    test:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"test"
+    }],
+    child:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"test"
+    }]
 })
